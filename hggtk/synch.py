ad3e638b51c079df73f9abd76ae5c151dac8321a
Steve Borho <steve@borho.org>
2007-12-17 11:33:45 -0600

    hggtk: switch synch.py to use HgThread
    Also removed redundant self.repo object.  It doubled the startup time
    for no apparent reason.

--- hggtk/synch.py
+++ hggtk/synch.py
@@ -15,32 +15,23 @@ import gtk
 import gobject
 import pango
 import Queue
-import subprocess
 import os
 import threading
 from mercurial import hg, ui, util 
 from mercurial.node import *
-from hglib import rootpath
 from dialog import error_dialog
+from hglib import HgThread
 
 class SynchDialog(gtk.Dialog):
-    def __init__(self, cwd='', repos=[], hgpath='hg'):
+    def __init__(self, cwd='', root = '', repos=[]):
         """ Initialize the Dialog. """
         rootbase = os.path.basename(cwd)
         gtk.Dialog.__init__(self, title="TortoiseHg Synchronize - %s" % rootbase,
                                   parent=None,
                                   flags=0,
                                   buttons=())
-        self.root = rootpath(cwd)
+        self.root = root
         self.selected_path = None
-        self._hgpath = hgpath
-        self.thread1 = None
-        self.queue = Queue.Queue()
-
-        try:
-            self.repo = hg.repository(ui.ui(), path=self.root)
-        except hg.RepoError:
-            return None
 
         self.set_default_size(600, 400)
 
@@ -203,7 +194,7 @@ class SynchDialog(gtk.Dialog):
         dialog.destroy()
         
     def _close_clicked(self, *args):
-        if self.thread1 and self.thread1.isAlive():
+        if threading.activeCount() != 1:
             error_dialog("Can't close now", "command is running")
         else:
             self.response(gtk.RESPONSE_CLOSE)
@@ -212,7 +203,7 @@ class SynchDialog(gtk.Dialog):
         return True
         
     def _response(self, widget, response_id):
-        if self.thread1 and self.thread1.isAlive():
+        if threading.activeCount() != 1:
             error_dialog("Can't close now", "command is running")
             widget.emit_stop_by_name('response')
         else:
@@ -271,27 +262,20 @@ class SynchDialog(gtk.Dialog):
         text_entry = self._pathbox.get_child()
         remote_path = str(text_entry.get_text())
         
-        self.cmdline = [self._hgpath] + cmd
-        self.cmdline.append('--verbose')
-        self.cmdline.append('--repository')
-        self.cmdline.append(self.repo.root)
-        self.cmdline.append(remote_path)
+        cmdline = cmd
+        cmdline.append('--verbose')
+        cmdline.append('--repository')
+        cmdline.append(self.root)
+        cmdline.append(remote_path)
         
         # show command to be executed
-        if type(self.cmdline) == type([]):
-            cmd = ' '.join(self.cmdline)
-        else:
-            cmd = self.cmdline
-        
-        # execute comnand and show output on text widget
         self.write("", False)
-        self.write("$ %s\n" % cmd)
-        self._start_thread()
+        self.write("$ %s\n" % ' '.join(cmdline))
 
-    def _start_thread(self):
+        # execute command and show output on text widget
         gobject.timeout_add(10, self.process_queue)
-        self.thread1 = threading.Thread(target=self._do_popen)
-        self.thread1.start()
+        self.hgthread = HgThread(cmdline)
+        self.hgthread.start()
         
     def write(self, msg, append=True):
         msg = unicode(msg, 'iso-8859-1')
@@ -305,48 +289,19 @@ class SynchDialog(gtk.Dialog):
         """
         Handle all the messages currently in the queue (if any).
         """
-        while self.queue.qsize():
+        while self.hgthread.getqueue().qsize():
             try:
-                msg = self.queue.get(0)
+                msg = self.hgthread.getqueue().get(0)
                 self.write(msg)
             except Queue.Empty:
                 pass
-                
-        return True
-        
-    def _do_popen(self):        
-        if not self.cmdline:
-            return
+        if threading.activeCount() == 1:
+            return False # Stop polling this function
+        else:
+            return True
 
-        # run hg in unbuffered mode, so the output can be captured and display a.s.a.p.
-        os.environ['PYTHONUNBUFFERED'] = "1"
-
-        # start hg operation on a subprocess and capture the output
-        pop = subprocess.Popen(self.cmdline, 
-                               shell=False,
-                               stderr=subprocess.STDOUT,
-                               stdout=subprocess.PIPE,
-                               stdin=subprocess.PIPE)
-
-        try:
-            line = 0
-            blocksize = 1024
-            while pop.poll() == None:
-                if line < 100:
-                    out = pop.stdout.readline()
-                    line += 1
-                else:
-                    out = pop.stdout.read(blocksize)
-                    if blocksize < 1024 * 50:
-                        blocksize *= 2
-                self.queue.put(out)
-            out = pop.stdout.read()
-            self.queue.put(out)
-        except IOError:
-            pass
-
-def run(cwd='', files=[], hgpath='hg', **opts):
-    dialog = SynchDialog(cwd, repos=files, hgpath=hgpath)
+def run(cwd='', root='', files=[], **opts):
+    dialog = SynchDialog(cwd, root, files)
     dialog.show_all()
     gtk.gdk.threads_init()
     gtk.gdk.threads_enter()
