b44e9b9f9084e6f0185c6c64de13544ed8155a3a
Steve Borho <steve@borho.org>
2008-01-21 12:39:43 -0600

    hggtk/changeset: minor cleanups

--- hggtk/changeset.py
+++ hggtk/changeset.py
@@ -82,7 +82,6 @@ class GChange(GDialog):
                 title += ':' + str(self.parents[1])
             else:
                 title += ':' + str(self.parents[0])
-            self.set_title(title)
         else:
             self.parent_toggle.set_sensitive(False)
             if self.parent_toggle.get_active():
@@ -91,12 +90,12 @@ class GChange(GDialog):
                 # so we exit out to prevent recursion.
                 self.parent_toggle.set_active(False)
                 return
-        self.set_title(title)
 
         ctx = self.repo.changectx(rev)
         if not ctx:
             self._last_rev = None
             return False
+        self.set_title(title)
         self.textview.freeze_child_notify()
         try:
             self._fill_buffer(self._buffer, rev, ctx, self._filelist)
@@ -150,8 +149,8 @@ class GChange(GDialog):
         out = StringIO.StringIO()
         patch.diff(self.repo, node1=parent, node2=ctx.node(),
                 files=ctx.files(), fp=out)
-        offset = eob.get_offset()
         difflines = util.tolocal(out.getvalue()).splitlines()
+        offset = eob.get_offset()
         fileoffs, tags, lines, statmax = self.prepare_diff(difflines, offset)
         buf.insert(eob, u''.join(lines).encode('utf-8'))
 
