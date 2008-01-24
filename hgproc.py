460b1f29c5240dc71e4e6ba6672e29df791e1b48
Steve Borho <steve@borho.org>
2008-01-23 21:42:16 -0600

    hgproc: add datamine to dialog list

--- hgproc.py
+++ hgproc.py
@@ -35,14 +35,15 @@ if not sys.stdin.isatty():
 # Map hgproc commands to dialog modules in hggtk/
 from hggtk import commit, status, addremove, tagadd, tags, history, merge
 from hggtk import diff, revisions, update, serve, clone, synch, hgcmd, about
-from hggtk import recovery, thgconfig
+from hggtk import recovery, thgconfig, datamine
 _dialogs = { 'commit' : commit,    'status' : status,    'revert' : status,
              'add'    : addremove, 'remove' : addremove, 'tag'    : tagadd,
              'tags'   : tags,      'log'    : history,   'history': history,
              'diff'   : diff,      'merge'  : merge,     'tip'    : revisions,
              'parents': revisions, 'heads'  : revisions, 'update' : update,
              'clone'  : clone,     'serve'  : serve,     'synch'  : synch,
-             'about'  : about,     'config' : thgconfig, 'recovery': recovery}
+             'about'  : about,     'config' : thgconfig, 'recovery': recovery,
+             'datamine': datamine}
 
 def get_list_from_file(filename):
     fd = open(filename, "r")
