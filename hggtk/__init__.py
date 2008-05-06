b1b3e3365b40d14b65746e1469a2f12e4cb70a86
TK Soh <teekaysoh@yahoo.com>
2008-05-06 04:16:38 +0100

    hggtk: fix trackback when icon path cannot be located
    applies to binaries built with py2exe

--- hggtk/__init__.py
+++ hggtk/__init__.py
@@ -4,4 +4,6 @@ import gtk
 
 # Default icon for apps which do not set one
 from shlib import get_tortoise_icon
-gtk.window_set_default_icon_from_file(get_tortoise_icon("hg.ico"))
+icon = get_tortoise_icon("hg.ico")
+if icon:
+    gtk.window_set_default_icon_from_file(icon)
