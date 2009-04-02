6f4e48d457893f063d1a16e48a7160a66243991b
Sune Foldager <sune.foldager@edlund.dk>
2009-04-02 10:51:54 -0500

    setup.py: fixes, additions and changes

--- setup.py
+++ setup.py
@@ -51,14 +51,28 @@ def setup_windows():
         _data_files = [(root, [os.path.join(root, file_) for file_ in files])
                             for root, dirs, files in os.walk('icons')]
         extra['windows'] = [
-                {"script":"tracelog.py",
+                {"script":"contrib/tracelog.py",
                             "icon_resources": [(1, "icons/tortoise/python.ico")]}
                 ]
         extra['com_server'] = ["tortoisehg"]
-        extra['console'] = ["contrib/hg", "contrib/hgtk"]
+        extra['console'] = ["contrib/hg", "hgtk"]
+
+    # add library files to support PyGtk-based dialogs/windows
+    includes = ['dbhash', 'pango', 'atk', 'pangocairo', 'cairo', 'gobject']
+
+    # Manually include other modules py2exe can't find by itself.
+    if 'hgext.highlight' in hgextmods:
+        includes += ['pygments.*', 'pygments.lexers.*', 'pygments.formatters.*',
+                     'pygments.filters.*', 'pygments.styles.*']
+    if 'hgext.patchbomb' in hgextmods:
+        includes += ['email.*', 'email.mime.*']
 
     extra['options'] = {
        "py2exe" : {
+           # This is one way to ensure that hgtk can find its icons when
+           # running in a py2exe environment. It also makes debugging easier.
+           "skip_archive" : 1,
+
            # Don't pull in all this MFC stuff used by the makepy UI.
            "excludes" : "pywin,pywin.dialogs,pywin.dialogs.list",
 
@@ -68,8 +82,8 @@ def setup_windows():
            #    the dist directory created by py2exe.
            #    also needed is the GTK's share/themes (as dist/share/themes), 
            #    for dialogs to display in MS-Windows XP theme.
-           "includes" : "dbhash,pango,atk,pangocairo,cairo,gobject," + \
-                        ",".join(hgextmods),
+           "includes" : includes + list(hgextmods),
+           "optimize" : 1,
        }
     }
 
@@ -92,10 +106,10 @@ def setup_posix():
 
 if os.name == "nt":
     (scripts, packages, data_files, extra) = setup_windows()
-    desc='Windows shell extension for Mercurial VCS',
+    desc='Windows shell extension for Mercurial VCS'
 else:
     (scripts, packages, data_files, extra) = setup_posix()
-    desc='TortoiseHg dialogs for Mercurial VCS',
+    desc='TortoiseHg dialogs for Mercurial VCS'
 
 try:
     l = os.popen('hg id -it').read().split()
