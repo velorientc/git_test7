4f737b8f8c19cffc2ca8d58513bcb72d855e328a
Steve Borho <steve@borho.org>
2009-04-08 22:23:31 -0500

    visdiff: refactor into standard run() type dialog
    Since the dialog is always launched, we can use the more well known
    layout of a run() function and a dialog class.

--- hggtk/hgtk.py
+++ hggtk/hgtk.py
@@ -296,9 +296,9 @@ def update(ui, **opts):
 
 def vdiff(ui, *pats, **opts):
     """launch configured visual diff tool"""
-    from hggtk.visdiff import visualdiff
+    from hggtk.visdiff import run
     repo = hg.repository(ui, path=hglib.rootpath(os.getcwd()))
-    visualdiff(repo, pats, opts)
+    run(repo, pats, **opts)
 
 ### help management, adapted from mercurial.commands.help_()
 def help_(ui, name=None, with_version=False):
--- hggtk/visdiff.py
+++ hggtk/visdiff.py
@@ -8,6 +8,7 @@
 # of the GNU General Public License, incorporated herein by reference.
 
 import gtk
+import gobject
 from mercurial.i18n import _
 from mercurial.node import short
 from mercurial import cmdutil, util, commands
@@ -21,17 +22,35 @@ try:
 except ImportError:
     openflags = 0
 
-diffpath, diffopts = None, None
+def snapshot_node(repo, files, node, tmproot):
+    '''snapshot files as of some revision'''
+    dirname = os.path.basename(repo.root)
+    if dirname == "":
+        dirname = "root"
+    dirname = '%s.%s' % (dirname, short(node))
+    base = os.path.join(tmproot, dirname)
+    os.mkdir(base)
+    ctx = repo[node]
+    for fn in files:
+        wfn = util.pconvert(fn)
+        if not wfn in ctx:
+            # skipping new file after a merge ?
+            continue
+        dest = os.path.join(base, wfn)
+        destdir = os.path.dirname(dest)
+        if not os.path.isdir(destdir):
+            os.makedirs(destdir)
+        data = repo.wwritedata(wfn, ctx[wfn].data())
+        open(dest, 'wb').write(data)
+    return dirname
 
 class FileSelectionDialog(gtk.Dialog):
     'Dialog for selecting visual diff candidates'
-    def __init__(self, title, modified, added, removed,
-            dir1, dir2, dir2root, tmproot):
+    def __init__(self, repo, pats, opts):
         'Initialize the Dialog'
         gtk.Dialog.__init__(self)
+        self.set_title('Visual Diffs')
         shlib.set_tortoise_icon(self, 'menushowchanged.ico')
-        self.set_title('Visual Diffs - ' + title)
-        self.diffs = (dir1, dir2, dir2root, tmproot)
         self.set_default_size(400, 150)
 
         scroller = gtk.ScrolledWindow()
@@ -60,21 +79,94 @@ class FileSelectionDialog(gtk.Dialog):
         fcol.add_attribute(cell, 'text', 1)
         treeview.append_column(fcol)
 
+        gobject.idle_add(self.find_files, repo, pats, opts, model)
+
+    def readtool(self, ui):
+        vdiff = ui.config('tortoisehg', 'vdiff', 'vdiff')
+        if not vdiff:
+            return '', None
+        for cmd, path in ui.configitems('extdiff'):
+            if cmd.startswith('cmd.'):
+                cmd = cmd[4:]
+                if cmd != vdiff:
+                    continue
+                if not path:
+                    path = cmd
+                diffopts = ui.config('extdiff', 'opts.' + cmd, '')
+                diffopts = diffopts and [diffopts] or []
+                return path, diffopts
+            elif cmd == vdiff:
+                # command = path opts
+                if path:
+                    diffopts = shlex.split(path)
+                    path = diffopts.pop(0)
+                else:
+                    path, diffopts = cmd, []
+                return path, diffopts
+        return '', None
+
+    def find_files(self, repo, pats, opts, model):
+        self.diffpath, self.diffopts = self.readtool(repo.ui)
+        if not self.diffpath:
+            Prompt(_('No visual diff tool'), 
+                   _('No tool has been configured'), self).run()
+            return
+
+        revs = opts.get('rev')
+        change = opts.get('change')
+
+        if change:
+            title = _('changeset ') + str(change)
+            node2 = repo.lookup(change)
+            node1 = repo[node2].parents()[0].node()
+        else:
+            if revs:
+                title = _('revision(s) ') + str(revs[0])
+            else:
+                title = _('working changes')
+            node1, node2 = cmdutil.revpair(repo, revs)
+
+        matcher = cmdutil.match(repo, pats, opts)
+        modified, added, removed = repo.status(node1, node2, matcher)[:3]
+        if not (modified or added or removed):
+            Prompt(_('No files to diff'), _('No modified files'), self).run()
+            return
+
+        self.set_title('Visual Diffs - ' + title)
+
+        tmproot = tempfile.mkdtemp(prefix='extdiff.')
+        self.connect('destroy', self.delete_tmproot, tmproot)
+        dir2 = ''
+        dir2root = ''
+        # Always make a copy of node1
+        dir1 = snapshot_node(repo, modified + removed, node1, tmproot)
+
+        # If node2 in not the wc or there is >1 change, copy it
+        if node2:
+            dir2 = snapshot_node(repo, modified + added, node2, tmproot)
+        else:
+            # This lets the diff tool open the changed file directly
+            dir2root = repo.root
+
+        self.dirs = (dir1, dir2, dir2root, tmproot)
+
         for m in modified:
-            treeview.get_model().append(['M', m])
+            model.append(['M', m])
         for a in added:
-            treeview.get_model().append(['A', a])
+            model.append(['A', a])
         for r in removed:
-            treeview.get_model().append(['R', r])
+            model.append(['R', r])
+
+    def delete_tmproot(self, _, tmproot):
+        shutil.rmtree(tmproot)
 
     def rowactivated(self, tree, path, column):
-        global diffpath, diffopts
         selection = tree.get_selection()
         if selection.count_selected_rows() != 1:
             return False
         model, paths = selection.get_selected_rows()
         st, fname = model[paths[0]]
-        dir1, dir2, dir2root, tmproot = self.diffs
+        dir1, dir2, dir2root, tmproot = self.dirs
         if st == 'A':
             dir1 = os.devnull
             dir2 = os.path.join(dir2root, dir2, util.localpath(fname))
@@ -84,113 +176,18 @@ class FileSelectionDialog(gtk.Dialog):
         else:
             dir1 = os.path.join(dir1, util.localpath(fname))
             dir2 = os.path.join(dir2root, dir2, util.localpath(fname))
-        cmdline = [diffpath] + diffopts + [dir1, dir2]
+        cmdline = [self.diffpath] + self.diffopts + [dir1, dir2]
         subprocess.Popen(cmdline, shell=False, cwd=tmproot,
                        creationflags=openflags,
                        stderr=subprocess.PIPE,
                        stdout=subprocess.PIPE,
                        stdin=subprocess.PIPE)
 
-def showfiles(title, modified, removed, added, dir1, dir2, dir2root, tmproot):
-    '''open a treeview dialog to allow user to select files'''
-    dialog = FileSelectionDialog(title, modified, added, removed,
-            dir1, dir2, dir2root, tmproot)
-    dialog.show_all()
+def run(repo, pats, **opts):
+    dialog = FileSelectionDialog(repo, pats, opts)
     dialog.connect('destroy', gtk.main_quit)
+    dialog.show_all()
     gtk.gdk.threads_init()
     gtk.gdk.threads_enter()
     gtk.main()
     gtk.gdk.threads_leave()
-
-def snapshot_node(repo, files, node, tmproot):
-    '''snapshot files as of some revision'''
-    dirname = os.path.basename(repo.root)
-    if dirname == "":
-        dirname = "root"
-    dirname = '%s.%s' % (dirname, short(node))
-    base = os.path.join(tmproot, dirname)
-    os.mkdir(base)
-    ctx = repo[node]
-    for fn in files:
-        wfn = util.pconvert(fn)
-        if not wfn in ctx:
-            # skipping new file after a merge ?
-            continue
-        dest = os.path.join(base, wfn)
-        destdir = os.path.dirname(dest)
-        if not os.path.isdir(destdir):
-            os.makedirs(destdir)
-        data = repo.wwritedata(wfn, ctx[wfn].data())
-        open(dest, 'wb').write(data)
-    return dirname
-
-def visualdiff(repo, pats, opts):
-    global diffpath, diffopts
-    if diffpath is None:
-        diffpath, diffopts = init(repo.ui)
-    if not diffpath:
-        Prompt(_('No visual diff tool'), 
-               _('No tool has been configured'), None).run()
-        return
-
-    revs = opts.get('rev')
-    change = opts.get('change')
-
-    if change:
-        title = _('changeset ') + str(change)
-        node2 = repo.lookup(change)
-        node1 = repo[node2].parents()[0].node()
-    else:
-        if revs:
-            title = _('revision(s) ') + str(revs[0])
-        else:
-            title = _('working changes')
-        node1, node2 = cmdutil.revpair(repo, revs)
-
-    matcher = cmdutil.match(repo, pats, opts)
-    modified, added, removed = repo.status(node1, node2, matcher)[:3]
-    if not (modified or added or removed):
-        Prompt(_('No files to diff'), _('No modified files'), None).run()
-        return
-
-    tmproot = tempfile.mkdtemp(prefix='extdiff.')
-    dir2 = ''
-    dir2root = ''
-    try:
-        # Always make a copy of node1
-        dir1 = snapshot_node(repo, modified + removed, node1, tmproot)
-        changes = len(modified) + len(removed) + len(added)
-
-        # If node2 in not the wc or there is >1 change, copy it
-        if node2:
-            dir2 = snapshot_node(repo, modified + added, node2, tmproot)
-        else:
-            # This lets the diff tool open the changed file directly
-            dir2root = repo.root
-        showfiles(title, modified, removed, added, dir1, dir2, dir2root, tmproot)
-    finally:
-        shutil.rmtree(tmproot)
-
-def init(ui):
-    vdiff = ui.config('tortoisehg', 'vdiff', 'vdiff')
-    if not vdiff:
-        return '', None
-    for cmd, path in ui.configitems('extdiff'):
-        if cmd.startswith('cmd.'):
-            cmd = cmd[4:]
-            if cmd != vdiff:
-                continue
-            if not path:
-                path = cmd
-            diffopts = ui.config('extdiff', 'opts.' + cmd, '')
-            diffopts = diffopts and [diffopts] or []
-            return path, diffopts
-        elif cmd == vdiff:
-            # command = path opts
-            if path:
-                diffopts = shlex.split(path)
-                path = diffopts.pop(0)
-            else:
-                path, diffopts = cmd, []
-            return path, diffopts
-    return '', None
