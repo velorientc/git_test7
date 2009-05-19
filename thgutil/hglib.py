d06d64e294208b545741fd4d2e23b511a600c072
Steve Borho <steve@borho.org>
2009-05-18 22:13:38 -0500

    hglib: remove two unused imports

--- thgutil/hglib.py
+++ thgutil/hglib.py
@@ -6,11 +6,9 @@ This software may be used and distributed according to the terms
 of the GNU General Public License, incorporated herein by reference.
 """
 
-import gtk
 import os
 import sys
 import traceback
-import Queue
 import shlib
 from mercurial import hg, ui, util, extensions, commands, hook
 
