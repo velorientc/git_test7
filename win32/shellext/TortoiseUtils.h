47de1c8d2de29697fbf99ce41c12ef5a454deac8
Sune Foldager <cryo@cyanite.org>
2009-05-31 22:41:28 +0200

    TortoiseUtils.h: Add guard around ARRAYSIZE macro

--- win32/shellext/TortoiseUtils.h
+++ win32/shellext/TortoiseUtils.h
@@ -14,7 +14,9 @@ extern CRITICAL_SECTION g_critSectionBreak;
 template <class T> T GET_SAFE( T a ) {EnterCriticalSection(&g_critSectionBreak); T z = a; LeaveCriticalSection(&g_critSectionBreak); return z;}
 template <class T> void SET_SAFE( T & a , T b) {EnterCriticalSection(&g_critSectionBreak); a = b; LeaveCriticalSection(&g_critSectionBreak);}
 
+#ifndef ARRAYSIZE
 #define ARRAYSIZE(a)    (sizeof(a)/sizeof(a[0]))
+#endif
 
 extern int  LocalToWideChar(LPWSTR pWide, LPTSTR pLocal, DWORD dwChars);
 extern int  WideCharToLocal(LPTSTR pLocal, LPWSTR pWide, DWORD dwChars);
