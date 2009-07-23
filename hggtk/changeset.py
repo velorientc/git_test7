1a4de916d2112a574ebeb8885c22a821ed619fc7
Yuki KODAMA <endflow.net@gmail.com>
2009-07-23 13:06:16 +0900

    changeset: remove redundant temporary variable

--- hggtk/changeset.py
+++ hggtk/changeset.py
@@ -484,10 +484,9 @@ class ChangeSet(gdialog.GDialog):
         wfile = util.localpath(self.curfile)
         wfile, ext = os.path.splitext(os.path.basename(wfile))
         filename = "%s@%d%s" % (wfile, self.currev, ext)
-        fd = gtklib.NativeSaveFileDialogWrapper(Title=_("Save file to"),
-                                         InitialDir=self.cwd,
-                                         FileName=filename)
-        result = fd.run()
+        result = gtklib.NativeSaveFileDialogWrapper(Title=_("Save file to"),
+                                             InitialDir=self.cwd,
+                                             FileName=filename).run()
         if result:
             q = Queue.Queue()
             cpath = util.canonpath(self.repo.root, self.cwd, self.curfile)
