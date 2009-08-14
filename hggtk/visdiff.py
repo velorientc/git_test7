913c562caeae5a53e061a6c39db82765788181f6
Steve Borho <steve@borho.org>
2009-08-13 22:49:12 -0500

    visdiff: fix 'diff to local' when using raw extdiff

--- hggtk/visdiff.py
+++ hggtk/visdiff.py
@@ -308,11 +308,8 @@ def run(ui, *pats, **opts):
         change = opts.get('change')
         if change:
             args.extend(['--change', str(change)])
-        if len(revs) == 1:
-            args.extend(['--change', str(revs[0])])
-        else:
-            for r in revs:
-                args.extend(['--rev', str(r)])
+        for r in revs:
+            args.extend(['--rev', str(r)])
         args.extend(pats)
         args.extend(opts.get('canonpats', []))
         if os.name == 'nt':
