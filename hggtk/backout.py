5f18e64fccf38bd071a1b60520ab027efb3fca94
Yuki KODAMA <endflow.net@gmail.com>
2009-08-29 17:16:59 +0900

    backout: use gtk.Dialog as base class

--- hggtk/backout.py
+++ hggtk/backout.py
@@ -17,15 +17,23 @@ from thgutil import hglib, paths
 
 from hggtk import changesetinfo, gtklib, hgcmd
 
-class BackoutDialog(gtk.Window):
+class BackoutDialog(gtk.Dialog):
     """ Backout effect of a changeset """
     def __init__(self, rev=None):
         """ Initialize the Dialog """
-        gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+        gtk.Dialog.__init__(self, title=_('Backout changeset - %s') % rev,
+                          buttons=(gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
+        gtklib.set_tortoise_icon(self, 'menurevert.ico')
         gtklib.set_tortoise_keys(self)
-
-        self.set_title(_('Backout changeset - ') + rev)
+        self.set_has_separator(False)
         self.set_default_size(600, 400)
+        self.connect('response', self.dialog_response)
+
+        # add Backout button
+        backoutbutton = gtk.Button(_('Backout'))
+        backoutbutton.connect('clicked', self.backout, rev)
+        self.action_area.pack_end(backoutbutton)
+
         self.notify_func = None
 
         try:
@@ -34,20 +42,17 @@ class BackoutDialog(gtk.Window):
             gobject.idle_add(self.destroy)
             return
 
-        vbox = gtk.VBox()
-        self.add(vbox)
-
         frame = gtk.Frame(_('Changeset Description'))
         revid, desc = changesetinfo.changesetinfo(repo, rev)
         frame.add(desc)
         frame.set_border_width(5)
-        vbox.pack_start(frame, False, False, 2)
+        self.vbox.pack_start(frame, False, False, 2)
 
         self.logview = gtk.TextView(buffer=None)
         self.logview.set_editable(True)
         self.logview.modify_font(pango.FontDescription('Monospace'))
-        buf = self.logview.get_buffer()
-        buf.set_text(_('Backed out changeset: ') + rev)
+        self.buf = self.logview.get_buffer()
+        self.buf.set_text(_('Backed out changeset: ') + rev)
         scrolledwindow = gtk.ScrolledWindow()
         scrolledwindow.set_shadow_type(gtk.SHADOW_ETCHED_IN)
         scrolledwindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
@@ -60,39 +65,23 @@ class BackoutDialog(gtk.Window):
         self.tips.set_tip(frame,
                 _('Commit message text for new changeset that reverses the'
                 '  effect of the change being backed out.'))
-        vbox.pack_start(frame, True, True, 4)
+        self.vbox.pack_start(frame, True, True, 4)
 
-        accelgroup = gtk.AccelGroup()
-        self.add_accel_group(accelgroup)
-        mod = gtklib.get_thg_modifier()
+        # prepare to show
+        backoutbutton.grab_focus()
 
-        hbbox = gtk.HButtonBox()
-        hbbox.set_layout(gtk.BUTTONBOX_END)
-        vbox.pack_start(hbbox, False, False, 2)
-
-        close = gtk.Button(_('Close'))
-        close.connect('clicked', lambda x: self.destroy())
-        key, modifier = gtk.accelerator_parse('Escape')
-        close.add_accelerator('clicked', accelgroup, key, 0,
-                gtk.ACCEL_VISIBLE)
-        hbbox.add(close)
-
-        backout = gtk.Button(_('Backout'))
-        key, modifier = gtk.accelerator_parse(mod+'Return')
-        backout.add_accelerator('clicked', accelgroup, key, modifier,
-                gtk.ACCEL_VISIBLE)
-        hbbox.add(backout)
-        backout.grab_focus()
-
-        backout.connect('clicked', self.backout, buf, rev)
+    def dialog_response(self, dialog, response_id):
+        if response_id == gtk.RESPONSE_CLOSE \
+                or response_id == gtk.RESPONSE_DELETE_EVENT:
+            self.destroy()
 
     def set_notify_func(self, func, *args):
         self.notify_func = func
         self.notify_args = args
 
-    def backout(self, button, buf, revstr):
-        start, end = buf.get_bounds()
-        msg = buf.get_text(start, end)
+    def backout(self, button, revstr):
+        start, end = self.buf.get_bounds()
+        msg = self.buf.get_text(start, end)
         cmdline = ['hg', 'backout', '--rev', revstr, '--message', hglib.fromutf(msg)]
         dlg = hgcmd.CmdDialog(cmdline)
         dlg.show_all()
