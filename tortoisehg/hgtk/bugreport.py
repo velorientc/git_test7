0166f6fc0b5beb202e56ddfc6d51742e62f8fdcb
Emmanuel Rosa <goaway1000@gmail.com>
2009-09-13 20:37:58 -0400

    bugreport: add ability to save bug report to a file

--- tortoisehg/hgtk/bugreport.py
+++ tortoisehg/hgtk/bugreport.py
@@ -12,13 +12,15 @@ import pango
 from mercurial import extensions
 from tortoisehg.util.i18n import _
 from tortoisehg.util import hglib, version
-from tortoisehg.hgtk import gdialog
+from tortoisehg.hgtk import gdialog, gtklib
 
 class BugReport(gdialog.GDialog):
     """GTK+ based dialog for displaying traceback info to the user in a
     cut-paste friendly manner.  And include a number of useful bit of
     information like version numbers, etc.
     """
+
+    __error_text__ = None
 
     def get_title(self):
         return _('TortoiseHg Bug Report')
@@ -41,24 +43,39 @@ class BugReport(gdialog.GDialog):
         hbbox = gtk.HButtonBox()
         hbbox.set_layout(gtk.BUTTONBOX_END)
         vbox.pack_start(hbbox, False, False, 2)
+        save = gtk.Button(_('Save as..'))
+        save.connect('clicked', self.save_report_clicked)
+        hbbox.add(save)
         close = gtk.Button(_('Close'))
         close.connect('clicked', gtk.main_quit)
         hbbox.add(close)
-
-        text = '{{{\n#!python\n' # Wrap in Bitbucket wiki preformat markers
-        text += _('** Please report this bug to'
-                ' tortoisehg-discuss@lists.sourceforge.net or'
-                ' http://bitbucket.org/tortoisehg/stable/issues\n')
-        text += '** Mercurial version (%s).  TortoiseHg version (%s)\n' % (
-                hglib.hgversion, version.version())
-        text += '** Command: %s\n' % (self.opts['cmd'])
-        text += '** CWD: %s\n' % os.getcwd()
-        extlist = [x[0] for x in extensions.extensions()]
-        text += '** Extensions loaded: %s\n' % ', '.join(extlist)
-        text += self.opts['error']
-        text += '\n}}}'
-        textview.get_buffer().set_text(text)
+        textview.get_buffer().set_text(self.get_error_text())
         return vbox
+
+    def get_error_text(self):
+        if self.__error_text__ == None:
+            text = '{{{\n#!python\n' # Wrap in Bitbucket wiki preformat markers
+            text += _('** Please report this bug to'
+                    ' tortoisehg-discuss@lists.sourceforge.net or'
+                    ' http://bitbucket.org/tortoisehg/stable/issues\n')
+            text += '** Mercurial version (%s).  TortoiseHg version (%s)\n' % (
+                    hglib.hgversion, version.version())
+            text += '** Command: %s\n' % (self.opts['cmd'])
+            text += '** CWD: %s\n' % os.getcwd()
+            extlist = [x[0] for x in extensions.extensions()]
+            text += '** Extensions loaded: %s\n' % ', '.join(extlist)
+            text += self.opts['error']
+            text += '\n}}}'
+            self.__error_text__ = text
+        return self.__error_text__ 
+
+    def save_report_clicked(self, button):
+        file_path = gtklib.NativeSaveFileDialogWrapper(Title = _('Save error report to')).run()
+
+        if file_path:
+            fd = file(file_path, 'w')
+            fd.write(self.get_error_text())
+            fd.close()
 
 def run(_ui, *pats, **opts):
     return BugReport(_ui, None, None, None, opts)
