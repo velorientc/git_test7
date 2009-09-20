da939ac92b2f550205d2903d3c7b5924d6b197c0
Steve Borho <steve@borho.org>
2009-09-20 09:53:36 -0500

    update: remove BOM

--- tortoisehg/hgtk/update.py
+++ tortoisehg/hgtk/update.py
@@ -1,4 +1,4 @@
-﻿# update.py - TortoiseHg's dialog for updating repo
+# update.py - TortoiseHg's dialog for updating repo
 #
 # Copyright 2007 TK Soh <teekaysoh@gmail.com>
 # Copyright 2007 Steve Borho <steve@borho.org>
