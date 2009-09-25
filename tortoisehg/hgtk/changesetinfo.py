94bd6078ee67d48b5585ea517a82272883e764ae
Yuki KODAMA <endflow.net@gmail.com>
2009-09-25 12:17:11 +0900

    changesetinfo: use LayoutTable

--- tortoisehg/hgtk/changesetinfo.py
+++ tortoisehg/hgtk/changesetinfo.py
@@ -16,7 +16,7 @@ from tortoisehg.util import hglib
 from tortoisehg.hgtk import gtklib
 
 def changesetinfo(repo, revid, head=False):
-    def lbl(str, bold=False, right=False):
+    def lbl(str, bold=True, right=True):
         str = gtklib.markup_escape_text(str)
         label = gtk.Label()
         if bold:
@@ -24,36 +24,28 @@ def changesetinfo(repo, revid, head=False):
         label.set_alignment((right and 1 or 0), 0)
         label.set_markup(str)
         return label
-    def addrow(table, header=None, value=None):
-        row = table.get_property('n-rows')
-        table.set_property('n-rows', row + 1)
-        if header:
-            if isinstance(header, str):
-                header = lbl(header, True, True)
-            table.attach(header, 0, 1, row, row + 1, gtk.FILL, 0, 4, 1)
-        if value:
-            if isinstance(value, str):
-                value = lbl(value)
-            table.attach(value, 1, 2, row, row + 1, gtk.FILL|gtk.EXPAND, 0, 4, 1)
+    def val(str, bold=False):
+        return lbl(str, bold=bold, right=False)
 
     # prepare data to display
-    table = gtk.Table(1, 2)
+    table = gtklib.LayoutTable()
+    table.set_default_paddings(ypad=1)
     ctx = repo[revid]
     revstr = str(ctx.rev())
-    summary = ctx.description().replace('\0', '').split('\n')[0]
+    summary = ctx.description().replace('\0', '').split('\n')[0][:80]
     node = repo.lookup(revid)
     tags = repo.nodetags(node)
 
     # construct gtk.Table
-    addrow(table, _('rev'), revstr)
-    addrow(table, _('summary'), hglib.toutf(summary[:80]))
-    addrow(table, _('user'), hglib.toutf(ctx.user()))
-    addrow(table, _('date'), hglib.displaytime(ctx.date()))
-    addrow(table, _('branch'), hglib.toutf(ctx.branch()))
+    table.add_row(lbl(_('rev')), val(revstr))
+    table.add_row(lbl(_('summary')), val(hglib.toutf(summary)))
+    table.add_row(lbl(_('user')), val(hglib.toutf(ctx.user())))
+    table.add_row(lbl(_('date')), val(hglib.displaytime(ctx.date())))
+    table.add_row(lbl(_('branch')), val(hglib.toutf(ctx.branch())))
     if tags:
-        addrow(table, _('tags'), hglib.toutf(', '.join(tags)))
+        table.add_row(lbl(_('tags')), val(hglib.toutf(', '.join(tags))))
     if head and node not in repo.heads():
-        addrow(table, value=lbl(_('Not a head revision!'), True))
+        table.add_row(None, val(_('Not a head revision!'), bold=True))
 
     # just for padding
     vbox = gtk.VBox()
