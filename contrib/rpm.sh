daa7852346774acaa49b650a63107cac8a07ebb9
Mads Kiilerich <mads@kiilerich.com>
2009-10-06 01:31:59 +0200

    contrib/rpm.sh: Exit if build fails

--- contrib/rpm.sh
+++ contrib/rpm.sh
@@ -2,6 +2,6 @@
 
 mkdir -p rpmbuild/{SOURCES,BUILD}
 hg archive -t tgz rpmbuild/SOURCES/tortoisehg-hg.tar.gz
-rpmbuild --define "_topdir $(pwd)/rpmbuild" -ba $(dirname $0)/tortoisehg.spec
+rpmbuild --define "_topdir $(pwd)/rpmbuild" -ba $(dirname $0)/tortoisehg.spec || exit 1
 rm -rf rpmbuild/BUILD/
 ls -l rpmbuild/{RPMS/*,SRPMS}/tortoisehg-*.rpm
