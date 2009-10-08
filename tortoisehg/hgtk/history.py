e2ed78eeb9a60d435935cc6d9140bed46a0b8c5c
Steve Borho <steve@borho.org>
2009-10-07 22:26:36 -0500

    history: use full 'changelog' name in window title
    The tool is called 'Changelog' in the tools menu.

--- tortoisehg/hgtk/history.py
+++ tortoisehg/hgtk/history.py
@@ -58,7 +58,7 @@ class GLog(gdialog.GDialog):
         self.exs = [ name for name, module in extensions.extensions() ]
 
     def get_title(self):
-        str = _('%s log') % self.get_reponame()
+        str = _('%s - changelog') % self.get_reponame()
         if self.bfile:
             str += _(' (Bundle Preview)')
         return str
