512387a181c290d20fccc5a69359c1dd743bb254
Steve Borho <steve@borho.org>
2009-12-29 22:46:48 -0600

    setup: add docdiff executable

--- setup.py
+++ setup.py
@@ -114,6 +114,8 @@ def setup_windows():
     extra['console'] = [
             {'script':'contrib/hg', 'icon_resources':[(0,'icons/hg.ico')]},
             {'script':'hgtk', 'icon_resources':[(0,'icons/thg_logo.ico')]}
+            {'script':'contrib/docdiff.py',
+             'icon_resources':[(0,'icons/TortoiseMerge.ico')]},
             ]
     extra['windows'] = [
             {'script':'thgtaskbar.py',
