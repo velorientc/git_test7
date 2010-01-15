2c1610308a37306198215e318f94c91a67f78e65
Yuki KODAMA <endflow.net@gmail.com>
2010-01-15 17:13:39 +0900

    thgmq: change direction of arrow icons in the toolbar

--- tortoisehg/hgtk/thgmq.py
+++ tortoisehg/hgtk/thgmq.py
@@ -98,23 +98,22 @@ class MQWidget(gtk.VBox):
 
         ## buttons
         self.btn = {}
-        popallbtn = tbar.append_button(gtk.STOCK_GOTO_FIRST,
+        popallbtn = tbar.append_button(gtk.STOCK_GOTO_TOP,
                                       _('Unapply all patches'))
         popallbtn.connect('clicked', self.popall_clicked)
         self.btn['popall'] = popallbtn
 
-        popbtn = tbar.append_button(gtk.STOCK_GO_BACK,
+        popbtn = tbar.append_button(gtk.STOCK_GO_UP,
                                    _('Unapply last patch'))
         popbtn.connect('clicked', self.pop_clicked)
         self.btn['pop'] = popbtn
 
-        pushbtn = gtk.ToolButton(gtk.STOCK_GO_FORWARD)
-        pushbtn = tbar.append_button(gtk.STOCK_GO_FORWARD,
+        pushbtn = tbar.append_button(gtk.STOCK_GO_DOWN,
                                     _('Apply next patch'))
         pushbtn.connect('clicked', self.push_clicked)
         self.btn['push'] = pushbtn
 
-        pushallbtn = tbar.append_button(gtk.STOCK_GOTO_LAST,
+        pushallbtn = tbar.append_button(gtk.STOCK_GOTO_BOTTOM,
                                        _('Apply all patches'))
         pushallbtn.connect('clicked', self.pushall_clicked)
         self.btn['pushall'] = pushallbtn
