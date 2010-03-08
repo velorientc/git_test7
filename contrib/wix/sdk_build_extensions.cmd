3847cb0894fb457d65cef26d116cc3d03c6d4559
Adrian Buehlmann <adrian@cadifra.com>
2010-03-08 22:55:35 +0100

    wix: remove unused THG_MSI_INSTALL

--- contrib/wix/sdk_build_extensions.cmd
+++ contrib/wix/sdk_build_extensions.cmd
@@ -18,7 +18,7 @@ cd win32\shellext
 call "%_SDKBIN_%\SetEnv.cmd" /xp /%4 /release
 set DEBUG=1
 set THG_PLATFORM=%4
-set THG_EXTRA_CPPFLAGS=/DTHG_MSI_INSTALL /DTHG_PRODUCT_ID=%5
+set THG_EXTRA_CPPFLAGS=/DTHG_PRODUCT_ID=%5
 set THG_EXTRA_RCFLAGS=/dTHG_VERSION_FIRST=%1 /dTHG_VERSION_SECOND=%2 /dTHG_VERSION_THIRD=%3 /dTHG_PRODUCT_ID="%5"
 nmake /f Makefile.nmake clean
 nmake /f Makefile.nmake
