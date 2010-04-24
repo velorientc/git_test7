16c87b21f6365a9337f3297be21d3037ba2d905b
Yuya Nishihara <yuya@tcha.org>
2010-04-24 19:11:03 +0900

    hgignore: display error message if failed to update .hgignore

--- tortoisehg/hgtk/hgignore.py
+++ tortoisehg/hgtk/hgignore.py
@@ -14,7 +14,7 @@ from mercurial import hg, ui, match, util, error
 from tortoisehg.util.i18n import _
 from tortoisehg.util import shlib, hglib, paths
 
-from tortoisehg.hgtk import gtklib, gdialog
+from tortoisehg.hgtk import gtklib, gdialog, dialog
 
 class HgIgnoreDialog(gtk.Window):
     'Edit a reposiory .hgignore file'
@@ -252,8 +252,9 @@ class HgIgnoreDialog(gtk.Window):
                                     createmode=None)
             f.writelines(out)
             f.rename()
-        except IOError:
-            pass
+        except IOError, e:
+            dialog.error_dialog(self, _('Unable to write .hgignore file'),
+                                hglib.tounicode(str(e)))
         shlib.shell_notify([self.ignorefile])
         if self.notify_func:
             self.notify_func()
