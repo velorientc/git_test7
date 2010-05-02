3a7e1bbd20956d63f3abe49842a9bbe7ffab22ea
Adrian Buehlmann <adrian@cadifra.com>
2010-05-02 18:03:03 +0200

    kill use of mx.DateTime
    the must have been joking at Logilab

--- tortoisehg/hgqt/__init__.py
+++ tortoisehg/hgqt/__init__.py
@@ -21,7 +21,6 @@
 import os
 import os.path as osp
 import sys
-import mx.DateTime as dt
 
 def should_rebuild(srcfile, pyfile):
     return not osp.isfile(pyfile) or osp.isfile(srcfile) and \
@@ -50,17 +49,11 @@ import workbench_rc
 
 _icons = {}
 def _load_icons():
-    t = dt.today()
-    x = t.month == 12 and t.day in (24,25)
     d = QtCore.QDir(':/icons')
     for icn in d.entryList():
         name, ext = osp.splitext(str(icn))
         if name not in _icons or ext == ".svg":
             _icons[name] = QtGui.QIcon(':/icons/%s' % icn)
-    if x:
-        for name in _icons:
-            if name.endswith('_x'):
-                _icons[name[:-2]] = _icons[name]
 
 def icon(name):
     """
