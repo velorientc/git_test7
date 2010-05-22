74c10819720b5fa2e25b87f96558db2c8a1bb090
Adrian Buehlmann <adrian@cadifra.com>
2010-05-22 11:34:27 +0200

    repowidget: insert some empty lines in setupUi()
    for better legibility

--- tortoisehg/hgqt/repowidget.py
+++ tortoisehg/hgqt/repowidget.py
@@ -225,8 +225,10 @@ class RepoWidget(QtGui.QWidget):
         self.horizontalLayout = QtGui.QHBoxLayout(self)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setMargin(0)
+
         self.revisions_splitter = QtGui.QSplitter(self)
         self.revisions_splitter.setOrientation(QtCore.Qt.Vertical)
+
         self.repoview = HgRepoView(self.revisions_splitter)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -234,6 +236,7 @@ class RepoWidget(QtGui.QWidget):
         sizePolicy.setHeightForWidth(self.repoview.sizePolicy().hasHeightForWidth())
         self.repoview.setSizePolicy(sizePolicy)
         self.repoview.setFrameShape(QtGui.QFrame.StyledPanel)
+
         self.frame_maincontent = QtGui.QFrame(self.revisions_splitter)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -242,9 +245,11 @@ class RepoWidget(QtGui.QWidget):
         self.frame_maincontent.setSizePolicy(sizePolicy)
         self.frame_maincontent.setFrameShape(QtGui.QFrame.NoFrame)
         self.frame_maincontent.setFrameShadow(QtGui.QFrame.Plain)
+
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.frame_maincontent)
         self.verticalLayout_2.setSpacing(0)
         self.verticalLayout_2.setMargin(0)
+
         self.filelist_splitter = QtGui.QSplitter(self.frame_maincontent)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -253,7 +258,9 @@ class RepoWidget(QtGui.QWidget):
         self.filelist_splitter.setSizePolicy(sizePolicy)
         self.filelist_splitter.setOrientation(QtCore.Qt.Horizontal)
         self.filelist_splitter.setChildrenCollapsible(False)
+
         self.tableView_filelist = HgFileListView(self.filelist_splitter)
+
         self.frame = QtGui.QFrame(self.filelist_splitter)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(1)
@@ -261,10 +268,12 @@ class RepoWidget(QtGui.QWidget):
         sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
         self.frame.setSizePolicy(sizePolicy)
         self.frame.setFrameShape(QtGui.QFrame.NoFrame)
+
         self.verticalLayout = QtGui.QVBoxLayout(self.frame)
         self.verticalLayout.setSpacing(0)
         self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetDefaultConstraint)
         self.verticalLayout.setMargin(0)
+
         self.message_splitter = QtGui.QSplitter(self.frame)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -278,6 +287,7 @@ class RepoWidget(QtGui.QWidget):
         self.message_splitter.setOrientation(QtCore.Qt.Vertical)
         self.message_splitter.setOpaqueResize(True)
         self.message = RevMessage(self.message_splitter)
+
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -288,6 +298,7 @@ class RepoWidget(QtGui.QWidget):
         font.setFamily("Courier")
         font.setPointSize(9)
         self.message.setFont(font)
+
         self.fileview = HgFileView(self.message_splitter)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -295,6 +306,7 @@ class RepoWidget(QtGui.QWidget):
         sizePolicy.setHeightForWidth(self.fileview.sizePolicy().hasHeightForWidth())
         self.fileview.setSizePolicy(sizePolicy)
         self.fileview.setMinimumSize(QtCore.QSize(0, 0))
+
         self.verticalLayout.addWidget(self.message_splitter)
         self.verticalLayout_2.addWidget(self.filelist_splitter)
         self.horizontalLayout.addWidget(self.revisions_splitter)
