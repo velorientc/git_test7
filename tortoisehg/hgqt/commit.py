64f0f7877ab182917ff256567dabe0c824082d2d
Adrian Buehlmann <adrian@cadifra.com>
2010-05-24 19:42:46 +0200

    commit: set a smaller vertical spacing in form layout

--- tortoisehg/hgqt/commit.py
+++ tortoisehg/hgqt/commit.py
@@ -62,6 +62,7 @@ class CommitWidget(QWidget):
         layout.addWidget(self.stwidget)
         self.setLayout(layout)
         form = QFormLayout()
+        form.setVerticalSpacing(3)
         form.setContentsMargins(3, 0, 9, 0)
         repo = self.stwidget.repo
         wctx = repo[None]
