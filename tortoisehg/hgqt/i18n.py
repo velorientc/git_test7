0881d92ae6c3e3abcfeb6194782ec9368f333479
Yuki KODAMA <endflow.net@gmail.com>
2010-05-28 18:21:36 +0900

    i18n: need to use gettext of hgqt/i18n for keepgettext

--- tortoisehg/hgqt/i18n.py
+++ tortoisehg/hgqt/i18n.py
@@ -6,7 +6,7 @@
 # GNU General Public License version 2, incorporated herein by reference.
 
 from tortoisehg.util.i18n import _ as _gettext
-from tortoisehg.util.i18n import agettext, keepgettext
+from tortoisehg.util.i18n import agettext
 
 def _(message):
     return unicode(_gettext(message), 'utf-8')
@@ -14,3 +14,7 @@ def _(message):
 class localgettext(object):
     def _(self, message):
         return agettext(message)
+
+class keepgettext(object):
+    def _(self, message):
+        return {'id': message, 'str': _(message)}
