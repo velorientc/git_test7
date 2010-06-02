9a2c02eb01361032c222eaef8fad975ae25e8530
Steve Borho <steve@borho.org>
2010-06-01 23:07:59 -0500

    quickop: adopt * imports of PyQt4

--- tortoisehg/hgqt/quickop.py
+++ tortoisehg/hgqt/quickop.py
@@ -9,7 +9,8 @@ import os
 
 from mercurial import hg, ui, cmdutil, util
 
-from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
 from tortoisehg.hgqt.i18n import _
 from tortoisehg.util import hglib, shlib, paths
@@ -21,11 +22,12 @@ LABELS = { 'add': (_('Checkmark files to add'), _('Add')),
            'revert': (_('Checkmark files to revert'), _('Revert')),
            'remove': (_('Checkmark files to remove'), _('Remove')),}
 
-class QuickOpDialog(QtGui.QDialog):
+class QuickOpDialog(QDialog):
     """ Dialog for performing quick dirstate operations """
     def __init__(self, command, pats, parent=None):
-        QtGui.QDialog.__init__(self, parent)
-        self.setWindowFlags(self.windowFlags() & ~QtCore.Qt.WindowContextHelpButtonHint)
+        QDialog.__init__(self, parent)
+        self.setWindowFlags(self.windowFlags() &
+                            ~Qt.WindowContextHelpButtonHint)
         self.pats = pats
 
         # Handle rm alias
@@ -38,11 +40,17 @@ class QuickOpDialog(QtGui.QDialog):
         os.chdir(repo.root)
         self.setWindowTitle('%s - hg %s' % (hglib.get_reponame(repo), command))
 
-        layout = QtGui.QVBoxLayout()
+        layout = QVBoxLayout()
         self.setLayout(layout)
 
-        lbl = QtGui.QLabel(LABELS[command][0])
-        layout.addWidget(lbl)
+        hbox = QHBoxLayout()
+        lbl = QLabel(LABELS[command][0])
+        slbl = QLabel()
+        hbox.addWidget(lbl)
+        hbox.addStretch(1)
+        hbox.addWidget(slbl)
+        self.status_label = slbl
+        layout.addLayout(hbox)
 
         types = { 'add'    : 'I?',
                   'forget' : 'MAR!C',
@@ -60,16 +68,14 @@ class QuickOpDialog(QtGui.QDialog):
 
         if self.command == 'revert':
             ## no backup checkbox
-            chk = QtGui.QCheckBox(_('Do not save backup files (*.orig)'))
+            chk = QCheckBox(_('Do not save backup files (*.orig)'))
             self.chk = chk
             layout.addWidget(chk)
 
-        BB = QtGui.QDialogButtonBox
-        bb = QtGui.QDialogButtonBox(BB.Ok|BB.Cancel)
-        self.connect(bb, QtCore.SIGNAL("accepted()"),
-                     self, QtCore.SLOT("accept()"))
-        self.connect(bb, QtCore.SIGNAL("rejected()"),
-                     self, QtCore.SLOT("reject()"))
+        BB = QDialogButtonBox
+        bb = QDialogButtonBox(BB.Ok|BB.Cancel)
+        self.connect(bb, SIGNAL("accepted()"), self, SLOT("accept()"))
+        self.connect(bb, SIGNAL("rejected()"), self, SLOT("reject()"))
         bb.button(BB.Ok).setDefault(True)
         bb.button(BB.Ok).setText(LABELS[command][1])
         layout.addWidget(bb)
@@ -83,41 +89,37 @@ class QuickOpDialog(QtGui.QDialog):
         cmd.setHidden(True)
         self.cmd = cmd
 
-        s = QtCore.QSettings()
+        s = QSettings()
         stwidget.restoreState(s.value('quickop/state').toByteArray())
         self.restoreGeometry(s.value('quickop/geom').toByteArray())
         self.stwidget = stwidget
 
-        self.connect(self.stwidget, QtCore.SIGNAL('errorMessage'),
-                     self.errorMessage)
-
-    def errorMessage(self, msg):
-        # TODO: Does not appear to work
-        self.cmd.pmon.set_text(msg)
+        self.connect(self.stwidget, SIGNAL('errorMessage'),
+                self.cmd.pmon.set_text)
 
     def keyPressEvent(self, event):
-        if event.key() in (QtCore.Qt.Key_Return, QtCore.Qt.Key_Enter):
-            if event.modifiers() == QtCore.Qt.ControlModifier:
+        if event.key() in (Qt.Key_Return, Qt.Key_Enter):
+            if event.modifiers() == Qt.ControlModifier:
                 self.accept()  # Ctrl+Enter
             return
-        elif event.key() == QtCore.Qt.Key_Escape:
+        elif event.key() == Qt.Key_Escape:
             self.reject()
             return
-        return super(QtGui.QDialog, self).keyPressEvent(event)
+        return super(QDialog, self).keyPressEvent(event)
 
     def commandStarted(self):
         self.cmd.setShown(True)
-        self.bb.button(QtGui.QDialogButtonBox.Ok).setEnabled(False)
+        self.bb.button(QDialogButtonBox.Ok).setEnabled(False)
 
     def commandFinished(self, wrapper):
-        self.bb.button(QtGui.QDialogButtonBox.Ok).setEnabled(True)
+        self.bb.button(QDialogButtonBox.Ok).setEnabled(True)
         if wrapper.data is not 0:
             self.cmd.show_output(True)
         else:
             self.reject()
 
     def commandCanceled(self):
-        self.bb.button(QtGui.QDialogButtonBox.Ok).setEnabled(True)
+        self.bb.button(QDialogButtonBox.Ok).setEnabled(True)
 
     def accept(self):
         cmdline = [self.command]
@@ -137,10 +139,10 @@ class QuickOpDialog(QtGui.QDialog):
         if self.cmd.core.is_running():
             self.cmd.core.cancel()
         else:
-            s = QtCore.QSettings()
+            s = QSettings()
             s.setValue('quickop/state', self.stwidget.saveState())
             s.setValue('quickop/geom', self.saveGeometry())
-            QtGui.QDialog.reject(self)
+            QDialog.reject(self)
 
 def run(ui, *pats, **opts):
     pats = hglib.canonpaths(pats)
