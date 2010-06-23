3b5ab75de37dfc3863c2aaac5ac78233cfed3515
Steve Borho <steve@borho.org>
2010-06-23 14:08:21 -0500

    status, chunks: try harder to prevent display of binary diffs
    When showing working changes, provide a working context to check_max_diff() so
    it can determine file size and binary status of newly added files.  Also, avoid
    binary diffs entirely since they are expensive to calculate (memory and CPU)
    and add no value.
    
    Closes #1322, #1288, #1262, #1113

--- tortoisehg/hgtk/status.py
+++ tortoisehg/hgtk/status.py
@@ -739,7 +739,11 @@ class GStatus(gdialog.GWindow):
         return (self._node1, self._node2)
 
     def get_ctx(self):
-        return self.repo[self._node1]
+        'Return current changectx or workingctx'
+        if self._node2 == None and not self.mqmode:
+            return self.repo[None]
+        else:
+            return self.repo[self._node1]
 
     def set_file_states(self, paths, state=True):
         for p in paths:
