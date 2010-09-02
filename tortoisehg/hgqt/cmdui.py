35aa4583172133ba80ece24928c0946305110281
Steve Borho <steve@borho.org>
2010-09-01 23:08:00 -0500

    cmdui: show running command with emphasis style, prefix with '> '

--- tortoisehg/hgqt/cmdui.py
+++ tortoisehg/hgqt/cmdui.py
@@ -137,7 +137,7 @@ class Core(QObject):
             self.thread.progressReceived.connect(self.progress_received)
             self.clearSignal.connect(self.output_text.clear)
         if self.display:
-            w = thread.DataWrapper((self.display, ''))
+            w = thread.DataWrapper(('> ' + self.display, 'control'))
             self.thread.outputReceived.emit(w)
         self.thread.start()
 
