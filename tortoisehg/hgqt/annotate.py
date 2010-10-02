56cb52f22f6d2a8a3e8eee1a73e2130d743a31b5
Yuya Nishihara <yuya@tcha.org>
2010-10-03 00:25:10 +0900

    annotate: clean up import lines

--- tortoisehg/hgqt/annotate.py
+++ tortoisehg/hgqt/annotate.py
@@ -5,10 +5,9 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2, incorporated herein by reference.
 
-import os
-import re
+import os, re
 
-from mercurial import ui, hg, error, commands, cmdutil, util
+from mercurial import ui, error, util
 
 from tortoisehg.hgqt import visdiff, qtlib, wctxactions, thgrepo, lexers
 from tortoisehg.util import paths, hglib, colormap
