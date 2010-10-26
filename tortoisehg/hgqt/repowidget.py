7237715af2d7ea5bcad1fb87014dde29b03b6b0f
Steve Borho <steve@borho.org>
2010-10-25 20:52:48 -0500

    repowidget: remove unused import of thgrepo

--- tortoisehg/hgqt/repowidget.py
+++ tortoisehg/hgqt/repowidget.py
@@ -20,7 +20,7 @@ from tortoisehg.hgqt.qtlib import geticon, getfont, QuestionMsgBox, InfoMsgBox
 from tortoisehg.hgqt.qtlib import CustomPrompt, SharedWidget, DemandWidget
 from tortoisehg.hgqt.repomodel import HgRepoListModel
 from tortoisehg.hgqt import cmdui, update, tag, backout, merge, visdiff
-from tortoisehg.hgqt import archive, thgimport, thgstrip, run, thgrepo, purge
+from tortoisehg.hgqt import archive, thgimport, thgstrip, run, purge
 from tortoisehg.hgqt import bisect, rebase, resolve
 
 from tortoisehg.hgqt.repofilter import RepoFilterBar
