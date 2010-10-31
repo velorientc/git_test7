ba2f8340ec086fdb4616c53d3988eba75a1fa9f7
Steve Borho <steve@borho.org>
2010-10-30 23:03:48 -0500

    rebase: add wc checks before rebase, use stbar label only
    working copy checks are not performed if a rebase is already in progress.

--- tortoisehg/hgqt/rebase.py
+++ tortoisehg/hgqt/rebase.py
@@ -14,11 +14,12 @@ from mercurial import util, merge as mergemod
 
 from tortoisehg.util import hglib
 from tortoisehg.hgqt.i18n import _
-from tortoisehg.hgqt import qtlib, csinfo, cmdui, resolve
+from tortoisehg.hgqt import qtlib, csinfo, cmdui, resolve, commit
 
 BB = QDialogButtonBox
 
 class RebaseDialog(QDialog):
+    showMessage = pyqtSignal(QString)
 
     def __init__(self, repo, parent, **opts):
         super(RebaseDialog, self).__init__(parent)
@@ -48,6 +49,7 @@ class RebaseDialog(QDialog):
         d = opts.get('dest', '.')
         dest = csinfo.create(self.repo, d, style, withupdate=True)
         destb.layout().addWidget(dest)
+        self.destcsinfo = dest
         self.layout().addWidget(destb)
 
         sep = qtlib.LabeledSeparator(_('Options'))
@@ -69,16 +71,11 @@ class RebaseDialog(QDialog):
         else:
             self.svnchk = None
 
-        sep = qtlib.LabeledSeparator(_('Status'))
-        self.layout().addWidget(sep)
-
-        self.reslabel = QLabel()
-        self.reslabel.linkActivated.connect(self.linkActivated)
-        self.layout().addWidget(self.reslabel)
-
         self.cmd = cmdui.Widget()
         self.cmd.commandFinished.connect(self.commandFinished)
         self.cmd.escapePressed.connect(self.reject)
+        self.showMessage.connect(self.cmd.stbar.showMessage)
+        self.cmd.stbar.linkActivated.connect(self.linkActivated)
         self.layout().addWidget(self.cmd, 2)
 
         bbox = QDialogButtonBox(BB.Cancel|BB.Ok)
@@ -94,11 +91,49 @@ class RebaseDialog(QDialog):
             for w in (srcb, destb, sep, self.keepchk, self.detachchk):
                 w.setHidden(True)
             self.cmd.show_output(True)
+        else:
+            self.showMessage.emit(_('Checking...'))
+            QTimer.singleShot(0, self.checkStatus)
 
         self.setMinimumWidth(480)
         self.setMaximumHeight(800)
         self.resize(0, 340)
         self.setWindowTitle(_('Rebase - %s') % self.repo.displayname)
+
+    def checkStatus(self):
+        repo = self.repo
+        class CheckThread(QThread):
+            def __init__(self, parent):
+                QThread.__init__(self, parent)
+                self.dirty = False
+
+            def run(self):
+                wctx = repo[None]
+                if len(wctx.parents()) > 1:
+                    self.dirty = True
+                elif wctx.dirty():
+                    self.dirty = True
+                else:
+                    ms = mergemod.mergestate(repo)
+                    unresolved = False
+                    for path in ms:
+                        if ms[path] == 'u':
+                            self.dirty = True
+                            break
+        def completed():
+            self.th.wait()
+            if self.th.dirty:
+                self.bbox.button(BB.Ok).setEnabled(False)
+                txt = _('Before rebase, you must <a href="commit">'
+                        '<b>commit</b></a> or <a href="discard">'
+                        '<b>discard</b></a> changes.')
+            else:
+                self.bbox.button(BB.Ok).setEnabled(True)
+                txt = _('You may continue the rebase')
+            self.showMessage.emit(txt)
+        self.th = CheckThread(self)
+        self.th.finished.connect(completed)
+        self.th.start()
 
     def rebase(self):
         self.keepchk.setEnabled(False)
@@ -129,7 +164,7 @@ class RebaseDialog(QDialog):
     def commandFinished(self, ret):
         self.repo.decrementBusyCount()
         if self.checkResolve() is False:
-            self.reslabel.setText(_('<h3>Rebase is complete</h3>'))
+            self.showMessage.emit(_('Rebase is complete'))
             self.bbox.button(BB.Ok).setText(_('Close'))
             self.bbox.accepted.disconnect(self.rebase)
             self.bbox.accepted.connect(self.accept)
@@ -138,14 +173,14 @@ class RebaseDialog(QDialog):
         ms = mergemod.mergestate(self.repo)
         for path in ms:
             if ms[path] == 'u':
-                txt = _('<h3>Rebase generated merge <b>conflicts</b> that must '
-                        'be <a href="resolve"><b>resolved</b></a></h3>')
+                txt = _('Rebase generated merge <b>conflicts</b> that must '
+                        'be <a href="resolve"><b>resolved</b></a>')
                 self.bbox.button(BB.Ok).setEnabled(False)
                 break
         else:
             self.bbox.button(BB.Ok).setEnabled(True)
-            txt = _('<h3>You may continue the rebase</h3>')
-        self.reslabel.setText(txt)
+            txt = _('You may continue the rebase')
+        self.showMessage.emit(txt)
 
         if os.path.exists(self.repo.join('rebasestate')):
             self.bbox.button(BB.Cancel).setEnabled(True)
@@ -159,7 +194,30 @@ class RebaseDialog(QDialog):
         if cmd == 'resolve':
             dlg = resolve.ResolveDialog(self.repo, self)
             dlg.exec_()
-        self.checkResolve()
+            self.checkResolve()
+        elif cmd == 'commit':
+            dlg = commit.CommitDialog([], dict(root=self.repo.root), self)
+            dlg.finished.connect(dlg.deleteLater)
+            dlg.exec_()
+            self.destcsinfo.update(self.repo[None])
+            self.checkStatus()
+        elif cmd == 'discard':
+            labels = [(QMessageBox.Yes, _('&Discard')),
+                      (QMessageBox.No, _('Cancel'))]
+            if not qtlib.QuestionMsgBox(_('Confirm Discard'), _('Discard'
+                     ' outstanding changes in working directory?'),
+                     labels=labels, parent=self):
+                return
+            def finished(ret):
+                self.repo.decrementBusyCount()
+                if ret == 0:
+                    self.checkStatus()
+            cmdline = ['update', '--clean', '--repository', self.repo.root,
+                       '--rev', '.']
+            self.runner = cmdui.Runner(_('Discard - TortoiseHg'), True, self)
+            self.runner.commandFinished.connect(finished)
+            self.repo.incrementBusyCount()
+            self.runner.run(cmdline)
 
     def reject(self):
         if os.path.exists(self.repo.join('rebasestate')):
