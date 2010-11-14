8bcae6392c3fb3a98a1985f1b9debbcff5d35f1e
Johan Samyn <johan.samyn@gmail.com>
2010-11-14 16:54:05 +0100

    qreorder: show patch summary
    move reponame from dialogtitle into widget

--- tortoisehg/hgqt/qreorder.py
+++ tortoisehg/hgqt/qreorder.py
@@ -7,6 +7,8 @@
 
 import os
 
+from hgext import mq
+
 from tortoisehg.hgqt import qtlib, thgrepo
 from tortoisehg.util import hglib, paths
 from tortoisehg.hgqt.i18n import _
@@ -24,9 +26,8 @@ class QReorderDialog(QDialog):
     def __init__(self, repo, parent=None):
         QDialog.__init__(self, parent)
 
-        self.setWindowTitle(_('Reorder Unapplied Patches - ') + \
-                            repo.displayname)
-        self.setWindowFlags(self.windowFlags() & \
+        self.setWindowTitle(_('Reorder Unapplied Patches'))
+        self.setWindowFlags(self.windowFlags() &
                             ~Qt.WindowContextHelpButtonHint)
 
         self.repo = repo
@@ -34,23 +35,62 @@ class QReorderDialog(QDialog):
         repo.repositoryChanged.connect(self.refresh)
 
         layout = QVBoxLayout()
-        layout.setMargin(0)
+        layout.setMargin(4)
         self.setLayout(layout)
+
+        hb = QHBoxLayout()
+        hb.setMargin(2)
+        lbl = QLabel(_('Repository:'))
+        hb.addWidget(lbl)
+        le = QLineEdit()
+        hb.addWidget(le)
+        le.setReadOnly(True)
+        le.setFont(qtlib.getfont('fontlist').font())
+        le.setText(repo.displayname)
+        le.setFocusPolicy(Qt.NoFocus)
+        layout.addLayout(hb)
+        hl = qtlib.LabeledSeparator('')
+        layout.addWidget(hl)
+
+        class PatchListWidget(QListWidget):
+            def __init__(self, parent=None, dialog=None):
+                QListWidget.__init__(self, parent)
+                self.dlg=dialog
+                self.setCurrentRow(0)
+            def focusInEvent(self, e):
+                i = self.item(self.currentRow())
+                if i:
+                    self.dlg.showSummary(i)
+                QListWidget.focusInEvent(self, e)
 
         ugb = QGroupBox(_('Unapplied Patches - drag to reorder'))
         ugb.setLayout(QVBoxLayout())
         ugb.layout().setContentsMargins(*(0,)*4)
-        self.ulw = QListWidget()
+        self.ulw = PatchListWidget(dialog=self)
         self.ulw.setDragDropMode(QListView.InternalMove)
         ugb.layout().addWidget(self.ulw)
+        self.ulw.currentItemChanged.connect(lambda:
+                self.showSummary(self.ulw.item(self.ulw.currentRow())))
         layout.addWidget(ugb)
 
         agb = QGroupBox(_('Applied Patches'))
         agb.setLayout(QVBoxLayout())
         agb.layout().setContentsMargins(*(0,)*4)
-        self.alw = QListWidget()
+        self.alw = PatchListWidget(dialog=self)
         agb.layout().addWidget(self.alw)
+        self.alw.currentItemChanged.connect(lambda:
+                self.showSummary(self.alw.item(self.alw.currentRow())))
         layout.addWidget(agb)
+
+        slbl = QLabel(_('Summary:'))
+        layout.addWidget(slbl)
+        self.summ = QTextEdit()
+        self.summ.setFont(qtlib.getfont('fontcomment').font())
+        self.summ.setMinimumWidth(500)  # min 80 chars
+        self.summ.setMaximumHeight(100)
+        self.summ.setReadOnly(True)
+        self.summ.setFocusPolicy(Qt.NoFocus)
+        layout.addWidget(self.summ)
 
         self.refresh()
 
@@ -63,6 +103,8 @@ class QReorderDialog(QDialog):
         bb.button(BB.Ok).setDefault(True)
         layout.addWidget(bb)
 
+        self.ulw.setFocus()
+
     def refresh(self):
         patchnames = self.repo.mq.series[:]
         applied = [p.name for p in self.repo.mq.applied]
@@ -73,7 +115,8 @@ class QReorderDialog(QDialog):
         for p in reversed(patchnames):
             item = QListWidgetItem(hglib.tounicode(p))
             if p in applied:
-                item.setFlags(Qt.ItemIsSelectable)
+                item.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+                item.setForeground(QColor(111,111,111)) # gray, like disabled
                 alw.addItem(item)
             else:
                 item.setFlags(Qt.ItemIsSelectable |
@@ -81,6 +124,12 @@ class QReorderDialog(QDialog):
                               Qt.ItemIsDragEnabled)
                 ulw.addItem(item)
         self.cached = patchnames, applied
+
+    def showSummary(self, item):
+        patchname = hglib.fromunicode(item.text())
+        q = self.repo.mq
+        txt = hglib.toutf('\n'.join(mq.patchheader(q.join(patchname)).message))
+        self.summ.setText(txt)
 
     def accept(self):
         lines = []
