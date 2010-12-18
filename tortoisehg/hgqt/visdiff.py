2de3e7db5478ba3652e7009ae04576f0e8be98b6
Steve Borho <steve@borho.org>
2010-12-18 16:21:04 -0600

    visdiff: enable read-only temp files even for Windows

--- tortoisehg/hgqt/visdiff.py
+++ tortoisehg/hgqt/visdiff.py
@@ -105,7 +105,7 @@ def snapshot(repo, files, ctx):
         f.close()
         if ctx.rev() is None:
             fns_and_mtime.append((dest, repo.wjoin(fn), os.path.getmtime(dest)))
-        elif os.name != 'nt':
+        else:
             # Make file read/only, to indicate it's static (archival) nature
             os.chmod(dest, stat.S_IREAD)
     return base, fns_and_mtime
