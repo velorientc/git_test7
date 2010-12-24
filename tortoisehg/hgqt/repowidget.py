6dfb1e3a9268703016b6f134e8f5371835cdeb44
Steve Borho <steve@borho.org>
2010-12-24 15:00:02 -0600

    repowidget: fix sync tab switches

--- tortoisehg/hgqt/repowidget.py
+++ tortoisehg/hgqt/repowidget.py
@@ -689,19 +689,19 @@ class RepoWidget(QWidget):
         return True
 
     def incoming(self):
-        self.taskTabsWidget.setCurrentIndex(self.syncwidget)
+        self.taskTabsWidget.setCurrentIndex(self.syncTabIndex)
         self.syncDemand.get().incoming()
 
     def pull(self):
-        self.taskTabsWidget.setCurrentIndex(self.syncwidget)
+        self.taskTabsWidget.setCurrentIndex(self.syncTabIndex)
         self.syncDemand.get().pull()
 
     def outgoing(self):
-        self.taskTabsWidget.setCurrentIndex(self.syncwidget)
+        self.taskTabsWidget.setCurrentIndex(self.syncTabIndex)
         self.syncDemand.get().outgoing()
 
     def push(self):
-        self.taskTabsWidget.setCurrentIndex(self.syncwidget)
+        self.taskTabsWidget.setCurrentIndex(self.syncTabIndex)
         self.syncDemand.get().push()
 
     def qpush(self):
