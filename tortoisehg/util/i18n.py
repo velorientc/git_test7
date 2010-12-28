88377e7be74160f7f75b608dad431f924ee396ba
Yuya Nishihara <yuya@tcha.org>
2010-12-29 00:47:43 +0900

    i18n: detect default language without posix-style env var on Windows
    On Windows, we rarely have LANG environment variable, but
    gettext.bindtextdomain() depends on it. Since locale.getdefaultlocale() works
    on Windows, we can use it as a fallback.

--- tortoisehg/util/i18n.py
+++ tortoisehg/util/i18n.py
@@ -5,12 +5,23 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2, incorporated herein by reference.
 
-import gettext, sys
+import gettext, os, locale
+from mercurial import util
 from tortoisehg.util import paths
+
+_localeenvs = ('LANGUAGE', 'LC_ALL', 'LC_MESSAGES', 'LANG')
+def _defaultlanguage():
+    if os.name != 'nt' or util.any(e in os.environ for e in _localeenvs):
+        return  # honor posix-style env var
+    # On Windows, system locale is provided by getdefaultlocale(), but
+    # gettext doesn't take it into account.
+    return locale.getdefaultlocale()[0]
 
 def setlanguage(lang=None):
     """Change translation catalog to the specified language"""
     global t
+    if not lang:
+        lang = _defaultlanguage()
     opts = {}
     if lang:
         opts['languages'] = (lang,)
