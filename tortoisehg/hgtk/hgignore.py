c2ed88189647c0ab24cc5b0c5e84324e51d28826
Steve Borho <steve@borho.org>
2011-02-13 09:07:11 -0600

    hgignore: catch common errors from repo.status() (closes #1767)

--- tortoisehg/hgtk/hgignore.py
+++ tortoisehg/hgtk/hgignore.py
@@ -224,7 +224,12 @@ class HgIgnoreDialog(gtk.Window):
     def refresh(self):
         hglib.invalidaterepo(self.repo)
         matcher = match.always(self.repo.root, self.repo.root)
-        unknown = self.repo.status(match=matcher, unknown=True)[4]
+        try:
+            unknown = self.repo.status(match=matcher, unknown=True)[4]
+        except (EnvironmentError, util.Abort), inst:
+            gdialog.Prompt(_('Error while reading status'),
+                           hglib.toutf(str(inst)), self).run()
+            return
         self.unkmodel.clear()
         for u in unknown:
             self.unkmodel.append([hglib.toutf(u), u])
