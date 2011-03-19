3a19f95dfdd848a7b755bccce37528ec0afa9cad
Michael De Wildt <devnull@localhost>
2011-03-20 09:57:25 +1100

    postreview: fixed issue on linux where the version variable always yielded an AttributeError

--- tortoisehg/hgqt/postreview.py
+++ tortoisehg/hgqt/postreview.py
@@ -36,30 +36,26 @@ class LoadReviewDataThread(QThread):
             msg = _("Invalid Settings - Please provide your ReviewBoard username")
         else:
             rb = extensions.find("reviewboard")
-            plugin_msg = _("Invalid reviewboard plugin. Please download the "
-                        "mercurial reviewboard plugin version 3.5 or higher "
-                        "from the website below.\n\n %s") % \
-                        u'http://bitbucket.org/mdelagra/mercurial-reviewboard/'
             try:
-                if float(rb.__version__[:3]) < 3.5:
-                    msg = plugin_msg
-                else:
-                    pwd = self.dialog.password
-                    #if we don't have a password send something here to skip
-                    #the cli getpass in the extension. We will set the password
-                    #later
-                    if not pwd:
-                        pwd = "None"
+                pwd = self.dialog.password
+                #if we don't have a password send something here to skip
+                #the cli getpass in the extension. We will set the password
+                #later
+                if not pwd:
+                    pwd = "None"
 
-                    self.reviewboard = rb.make_rbclient(self.dialog.server,
-                                                        self.dialog.user,
-                                                        pwd)
-                    self.loadCombos()
+                self.reviewboard = rb.make_rbclient(self.dialog.server,
+                                                    self.dialog.user,
+                                                    pwd)
+                self.loadCombos()
 
             except rb.ReviewBoardError, e:
                 msg = e.msg
-            except AttributeError:
-                msg = plugin_msg
+            except TypeError:
+                msg = _("Invalid reviewboard plugin. Please download the "
+                        "mercurial reviewboard plugin version 3.5 or higher "
+                        "from the website below.\n\n %s") % \
+                        u'http://bitbucket.org/mdelagra/mercurial-reviewboard/'
 
         self.dialog.error_message = msg
 
