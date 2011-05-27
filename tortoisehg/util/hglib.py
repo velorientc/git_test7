0b7db38cbe073a22043831d0ce493d820dee6fea
Angel Ezquerra <angel.ezquerra@gmail.com>
2011-05-27 19:04:05 +0200

    hglib: added setConfigValue() function This function lets you set a config value on an ini file.

--- tortoisehg/util/hglib.py
+++ tortoisehg/util/hglib.py
@@ -16,6 +16,9 @@ from mercurial import encoding, templatefilters, filemerge, error
 from mercurial import demandimport, revset
 from mercurial import dispatch as hgdispatch
 
+from tortoisehg.hgqt import qtlib
+from tortoisehg.util import wconfig
+
 demandimport.disable()
 try:
     # hg >= 1.9
@@ -719,3 +722,24 @@ def dispatch(ui, args):
     else:
         # hg <= 1.8
         return hgdispatch._dispatch(ui, args)
+
+def setConfigValue(rcfilepath, cfgpath, value):
+    '''
+    rcpfilepath - absolute path to a configuration file
+    cfpath - full path of a configurable, ex 'web.name'
+    value - string value or None. if None, the configurable is removed
+    '''
+    fn, cfg = qtlib.loadIniFile([rcfilepath])
+    if not hasattr(cfg, 'write'):
+        return False
+    if fn is None:
+        return False
+    cfgpath = cfgpath.split('.')
+    if len(cfgpath) < 2:
+        return False
+    cfg.set(cfgpath[0], cfgpath[1], value)
+    try:
+        wconfig.writefile(cfg, fn)
+    except EnvironmentError, e:
+        return False
+    return True
