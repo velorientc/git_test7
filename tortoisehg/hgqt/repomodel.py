a27525f946ae67a2fb9bc5fd468c18e91948c3b8
Steve Borho <steve@borho.org>
2011-06-01 09:52:59 -0500

    repomodel: check for out-of-range rows in flags() method (fixes #754)

--- tortoisehg/hgqt/repomodel.py
+++ tortoisehg/hgqt/repomodel.py
@@ -461,9 +461,10 @@ class HgRepoListModel(QAbstractTableModel):
             return Qt.ItemFlags(0)
         if not self.revset:
             return Qt.ItemIsSelectable | Qt.ItemIsEnabled
-
         row = index.row()
         self.ensureBuilt(row=row)
+        if row >= len(self.graph):
+            return Qt.ItemFlags(0)
         gnode = self.graph[row]
         ctx = self.repo.changectx(gnode.rev)
 
