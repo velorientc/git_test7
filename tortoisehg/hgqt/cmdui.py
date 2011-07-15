b648c979cd49918449e043848881b5e9ae1f8d5e
David Golub <davidg@fogcreek.com>
2011-07-15 14:04:52 -0400

    cmdui: improve appearance of status bar control

--- tortoisehg/hgqt/cmdui.py
+++ tortoisehg/hgqt/cmdui.py
@@ -77,6 +77,7 @@ class ThgStatusBar(QStatusBar):
         self.lbl = QLabel()
         self.lbl.linkActivated.connect(self.linkActivated)
         self.addWidget(self.lbl)
+        self.setStyleSheet('QStatusBar::item { border: none }')
 
     @pyqtSlot(unicode)
     def showMessage(self, ustr):
