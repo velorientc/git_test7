d7d6e46f81a82f6bee9778fbe190a3057cbf12c1
David Golub <davidg@fogcreek.com>
2011-08-03 10:24:52 -0400

    patchctx: added stub kbfiles methods
    The hasattr check in filelistmodel is now no longer needed.

--- tortoisehg/hgqt/filelistmodel.py
+++ tortoisehg/hgqt/filelistmodel.py
@@ -144,8 +144,7 @@ class HgFileListModel(QAbstractTableModel):
         for lst, flag in ((added, 'A'), (modified, 'M'), (removed, 'R')):
             for f in filter(func, lst):
                 wasmerged = ismerge and f in ctxfiles
-                if hasattr(self._ctx, 'removeKbf'):
-                    f = self._ctx.removeKbf(f)
+                f = self._ctx.removeKbf(f)
                 files.append({'path': f, 'status': flag, 'parent': parent,
                               'wasmerged': wasmerged})
         return files
