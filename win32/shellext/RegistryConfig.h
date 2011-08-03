ee54cc191a901abf02eab8cc034e931206396f87
Adrian Buehlmann <adrian@cadifra.com>
2011-08-03 10:44:04 +0200

    remove win32/shellext
    The shell extension sources are now tracked in a separate repo in
    https://bitbucket.org/tortoisehg/shellext

--- win32/shellext/RegistryConfig.h
+++
@@ -1,8 +0,0 @@
-#ifndef REGISTRY_CONFIG_H_
-#define REGISTRY_CONFIG_H_
-
-#include <string>
-
-int GetRegistryConfig(const std::string& name, std::string& res);
-
-#endif
--- win32/shellext/create-parentid-h.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-hg parents --template "#define THG_PARENT_ID {node|short}\n" > parentid.h
