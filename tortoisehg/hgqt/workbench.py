258559261e681a4827ca85c9edee9d536391178a
Andrei Polushin <polushin@gmail.com>
2011-12-12 05:13:15 +0700

    i18n: make 'Goto revision' dialog translatable.

--- tortoisehg/hgqt/workbench.py
+++ tortoisehg/hgqt/workbench.py
@@ -728,8 +728,8 @@ class Workbench(QMainWindow):
             w.repoview.model().loadall()
 
     def gotorev(self):
-        rev, ok = QInputDialog.getText(self, "Goto revision",
-                                       "Enter revision identifier",
+        rev, ok = QInputDialog.getText(self, _("Goto revision"),
+                                       _("Enter revision identifier"),
                                        flags=Qt.WindowTitleHint)
         w = self.repoTabsWidget.currentWidget()
         if ok and w:
