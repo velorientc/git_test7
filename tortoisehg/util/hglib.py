4a77ad5fff97a88fb19fc7edb2d61d70ac131418
Yuya Nishihara <yuya@tcha.org>
2012-01-24 00:22:48 +0900

    hglib: remove compatibility layer for match and matchall of hg 1.8

--- tortoisehg/util/hglib.py
+++ tortoisehg/util/hglib.py
@@ -19,12 +19,6 @@ from mercurial import dispatch as hgdispatch
 
 demandimport.disable()
 try:
-    # hg >= 1.9
-    from mercurial.scmutil import match, matchall
-except (ImportError, AttributeError):
-    # hg <= 1.8
-    from mercurial.cmdutil import match, matchall
-try:
     # hg >= 2.1 (0bd17a4bed88)
     from mercurial.copies import mergecopies, pathcopies
 except (ImportError, AttributeError):
