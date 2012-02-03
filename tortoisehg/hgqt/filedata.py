012a064ef2137048088bcee784d3a891dcadcf20
Angel Ezquerra <devnull@localhost>
2012-02-03 13:36:35 +0100

    filedata: improve the status message shown when a subrepo is dirty.

--- tortoisehg/hgqt/filedata.py
+++ tortoisehg/hgqt/filedata.py
@@ -252,7 +252,7 @@ class FileData(object):
                     commands.status(_ui, srepo, modified=True, added=True, removed=True, deleted=True)
                     data = _ui.popbuffer()
                     if data:
-                        out.append(_('File Status:') + u'\n')
+                        out.append(_('The subrepository is dirty.') + u' ' + _('File Status:') + u'\n')
                         out.append(hglib.tounicode(data))
                         out.append(u'\n')
 
