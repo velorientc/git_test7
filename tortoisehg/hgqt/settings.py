36261810f18fdc022b9393e0c672c9fc4971d996
Angel Ezquerra <angel.ezquerra@gmail.com>
2012-02-04 13:17:32 +0100

    settings: add "Secret MQ Patches" entry to Commit settings panel
    The Commit panel is the one where this settings fits better, although it is not
    exactly a perfect match.

--- tortoisehg/hgqt/settings.py
+++ tortoisehg/hgqt/settings.py
@@ -30,6 +30,11 @@ def hasExtension(extname):
             return True
     return False
 
+# Detect if hg >= 2.1
+def phasesSupport():
+    import commands
+    return hasattr(commands, 'phase')
+
 class SettingsCombo(QComboBox):
     def __init__(self, parent=None, **opts):
         QComboBox.__init__(self, parent, toolTip=opts['tooltip'])
@@ -603,6 +608,9 @@ INFO = (
          'environment variables are set to a non-English language. '
          'This setting is used by the Merge, Tag and Backout dialogs. '
          'Default: False')),
+    _fi(_('Secret MQ Patches'), 'mq.secret', genBoolRBGroup,
+       _('Make MQ patches secret (instead of draft). '
+         'Default: False'), visible=phasesSupport),
     _fi(_('Monitor working<br>directory changes'),
         'tortoisehg.refreshwdstatus',
         (genDefaultCombo,
