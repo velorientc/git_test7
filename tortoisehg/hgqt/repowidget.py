0a0bf24460febb90b54192da68593bb49728ac73
Yuya Nishihara <yuya@tcha.org>
2012-09-05 22:29:56 +0900

    repowidget: switch to outgoing context menu only if outgoing nodes exist
    Without this change, if there's no outgoing changeset, outgoingMode is never
    switched off.

--- tortoisehg/hgqt/repowidget.py
+++ tortoisehg/hgqt/repowidget.py
@@ -548,6 +548,7 @@ class RepoWidget(QWidget):
     def setOutgoingNodes(self, nodes):
         self.filterbar.revsetle.setText('outgoing()')
         self.setRevisionSet([self.repo[n].rev() for n in nodes])
+        self.outgoingMode = True
 
         w = self.setInfoBar(qtlib.ConfirmInfoBar,
                             _('%d outgoing changesets') % len(nodes))
@@ -1075,7 +1076,6 @@ class RepoWidget(QWidget):
         self.syncDemand.get().pull()
     def outgoing(self):
         self.syncDemand.get().outgoing()
-        self.outgoingMode = True
     def push(self, confirm=True, **kwargs):
         """Call sync push.
 
