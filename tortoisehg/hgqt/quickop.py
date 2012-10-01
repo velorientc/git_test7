9e7ea819ffba4bf8f7ec7ea98b372e74c6ebb78c
Steve Borho <steve@borho.org>
2012-10-01 15:10:12 -0500

    quickop: remove trailing white-space

--- tortoisehg/hgqt/quickop.py
+++ tortoisehg/hgqt/quickop.py
@@ -51,7 +51,7 @@ class QuickOpDialog(QDialog):
         toplayout = QVBoxLayout()
         toplayout.setContentsMargins(5, 5, 5, 0)
         layout.addLayout(toplayout)
-        
+
         hbox = QHBoxLayout()
         lbl = QLabel(LABELS[command][0])
         slbl = QLabel()
@@ -115,7 +115,7 @@ class QuickOpDialog(QDialog):
         hbox.addWidget(bb)
         toplayout.addLayout(hbox)
         self.bb = bb
-        
+
         if self.command == 'add':
             if 'largefiles' in self.repo.extensions():
                 self.addLfilesButton = QPushButton(_('Add &Largefiles'))
