98fc92f732e4727229dae6eb3c743a10a68252c8
Yuya Nishihara <yuya@tcha.org>
2013-01-05 12:36:02 +0900

    tests: repotreemodel: inline thg-reporegistry.xml data for readability

--- tests/qt_repotreemodel_test.py
+++ tests/qt_repotreemodel_test.py
@@ -1,20 +1,38 @@
-import os
+from PyQt4.QtCore import QByteArray, QBuffer, QIODevice
 from nose.tools import *
 from mercurial import node
 from tortoisehg.hgqt.repotreemodel import *
 
-FIXTURES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
+def with_qbuffer(data='', mode=QIODevice.ReadOnly):
+    def decorate(func):
+        def newfunc():
+            ba = QByteArray(data)  # keep reference to avoid GC
+            f = QBuffer(ba)
+            f.open(mode)
+            try:
+                func(f)
+            finally:
+                f.close()
+        return make_decorator(func)(newfunc)
+    return decorate
 
-def openfixture(name, mode=QIODevice.ReadOnly):
-    path = os.path.join(FIXTURES_DIR, name)
-    f = QFile(path)
-    f.open(mode)
-    return f
+full_data = r'''<?xml version="1.0" encoding="UTF-8"?>
+<reporegistry>
+  <treeitem>
+    <allgroup>
+      <repo root="/thg" shortname="thg" basenode="bac32db38e52fd49acb62b94730a55f4f4b0cdee"/>
+      <repo root="/mercurial" shortname="hg" basenode="9117c6561b0bd7792fa13b50d28239d51b78e51f"/>
+    </allgroup>
+    <group name="bar">
+      <repo root="/python-vcs" shortname="python-vcs" basenode="b986218ba1c9b0d6a259fac9b050b1724ed8e545"/>
+    </group>
+  </treeitem>
+</reporegistry>
+'''
 
-def test_iterrepoitemfromxml():
-    f = openfixture('reporegistry.xml')
+@with_qbuffer(full_data)
+def test_iterrepoitemfromxml(f):
     repos = list(iterRepoItemFromXml(f))
-    f.close()
     assert_equals(['/thg', '/mercurial', '/python-vcs'],
                   map(lambda e: e.rootpath(), repos))
     assert_equals('thg', repos[0].shortname())
