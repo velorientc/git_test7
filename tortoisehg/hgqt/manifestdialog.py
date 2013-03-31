548b6fdc0201fb9b7b11c2c7d91303376103c67f
Yuya Nishihara <yuya@tcha.org>
2013-03-31 17:32:56 +0900

    manifest: report invalid --line option as error instead of traceback

--- tortoisehg/hgqt/manifestdialog.py
+++ tortoisehg/hgqt/manifestdialog.py
@@ -410,6 +410,14 @@ def run(ui, *pats, **opts):
         qtlib.ErrorMsgBox(_('Failed to open Manifest dialog'),
                           hglib.tounicode(e.message))
         return
+    try:
+        line = opts.get('line') and int(opts['line']) or None
+    except ValueError:
+        qtlib.ErrorMsgBox(_('Failed to open Manifest dialog'),
+                          _('The specified line number "%s" is invalid.')
+                          % hglib.tounicode(opts.get('line')))
+        return
+
     dlg = ManifestDialog(repo, rev)
 
     # set initial state after dialog visible
@@ -421,7 +429,6 @@ def run(ui, *pats, **opts):
                 path = opts['canonpath']
             else:
                 return
-            line = opts.get('line') and int(opts['line']) or None
             dlg.setSource(hglib.tounicode(path), rev, line)
             if opts.get('pattern'):
                 dlg.setSearchPattern(opts['pattern'])
