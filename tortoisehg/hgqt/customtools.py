cc6f4b2dbb9f3703c53a296d81e37e193013d28b
Wagner Bruna <wbruna@yahoo.com>
2013-04-14 10:57:27 -0300

    customtools: fix typo in setting documentation

--- tortoisehg/hgqt/customtools.py
+++ tortoisehg/hgqt/customtools.py
@@ -441,7 +441,7 @@ class CustomToolConfigDialog(QDialog):
         self.workingdir = self._addConfigItem(vbox, _('Working Directory'),
             QLineEdit(workingdir),
             _('The directory where the command will be be executed.\n'
-            'If this is not set, the root of the current repository'
+            'If this is not set, the root of the current repository '
             'will be used instead.\n'
             'You can use the same {VARIABLES} as on the "Command" setting.\n'))
         self.label = self._addConfigItem(vbox, _('Tool label'),
