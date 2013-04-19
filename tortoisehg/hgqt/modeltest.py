3b43d226fdef707d0683b193eb199c7b791a8f8a
Andr? Sintzoff <andre.sintzoff@gmail.com>
2013-04-19 20:59:34 +0200

    modeltest: preincrement correctly depth variable (fix 5a934f36b2aa)
    Thanks to Andreas Tscharner

--- tortoisehg/hgqt/modeltest.py
+++ tortoisehg/hgqt/modeltest.py
@@ -443,7 +443,8 @@ class ModelTest(QtCore.QObject):
                 # recursively go down the children
                 if self.model.hasChildren(index) and depth < 10:
                     # qDebug() << r << c << "hasChildren" << self.model.rowCount(index)
-                    self.checkChildren(index, depth+1)
+                    depth += 1
+                    self.checkChildren(index, depth)
                 #else:
                 #   if depth >= 10:
                 #       qDebug() << "checked 10 deep"
